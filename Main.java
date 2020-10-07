f22de0b9fb13a11eef8bffc78d75eb8128a51d23
zesha5816 <zesha5816@wrdsb.ca>
2020-10-07 14:12:29 +0000

    finished the example question

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * 
+ * @author Ahmad Zeshan
  */
 public class Main {
 
@@ -9,7 +10,19 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create the Scanner
+		Scanner input = new Scanner(System.in);
+		//output the message to the user
+		System.out.println("Please enter a number between 1 and 10");
+		//get input form user
+		int userInput = input.nextInt();
+		//increases value of i by an increment of 1 and keeps going until is reaches the userInput
+		for (int i = 0; i < userInput; i++){
+			//outputs stars on the same line
+			System.out.print("*");
+		}
+		//to oiutput the final space on another line
+		System.out.println("");
     
   }
 }
